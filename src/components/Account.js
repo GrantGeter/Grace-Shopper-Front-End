e69fbbddd3db01c6fc5d01eeb5e06cabaa7581ff
grant <grantgeter@gmail.com>
2021-03-28 13:15:50 -0500

    set up react router

---
+++ src/components/Account.js
@@ -0,0 +1,7 @@
+const Account = () => {
+    return (
+        <h3>Account Page</h3>
+    )
+}
+
+export default Account;
---
+++ src/components/Products.js
@@ -0,0 +1,7 @@
+const Products = () => {
+    return (
+        <h3>Products Page</h3>
+    )
+}
+
+export default Products;
