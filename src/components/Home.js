be366195a8e7bf566762fb423c1fc229c5a0a743
Kyle <kyle@daquin.com>
2021-04-05 21:03:09 -0500

    Finishing up styling and adding add to cart

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,45 @@
+import { useState, useEffect } from 'react'
+import { getProducts } from '../api';
+
 const Home = () => {
+
+const [products, setProducts] = useState([]);
+
+console.log(useEffect(() => {
+    getProducts()
+        .then(response => {
+            setProducts(response.data);
+        })
+}, []))
+console.log(getProducts())
+
     return (
-        <h3>Home Page</h3>
+
+        <div className="homePage">
+
+        <h3>Welcome to Grace Shopper!</h3>
+
+
+         <h4>Our Featured Products!</h4>
+ 
+  <div className="featuredProducts">
+                {
+            products.map((product, index) => {
+                        return (
+                            <div className='product' key={index}>
+                                {/* photo */}
+                                 <h4>{product.name}</h4>
+                                <p>{product.description}</p>
+                                <p>Price- {product.price}</p>
+                                <p>Category- {product.category}</p>                                
+                            </div>
+                        )
+                    })
+                }
+            </div> 
+
+        </div>
+      
     )
 }
 
